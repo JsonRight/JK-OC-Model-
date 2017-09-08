5b2d4dbd7885952095990d59a99a2f9a848b3592
姜奎 <jk601539674@foxmail.com>
2017-09-08 09:51:16 +0800

    firstcommit

---
+++ JsonForModel/HomeVC.h
@@ -0,0 +1,20 @@
+//
+//  HomeVC.h
+//  JsonForModel
+//
+//  Created by jk on 2016/12/29.
+//  Copyright © 2016年 jk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface HomeVC : NSViewController
+@property (nonatomic , strong) IBOutlet NSButton *mj_btn;
+@property (nonatomic , strong) IBOutlet NSButton *yy_btn;
+@property (nonatomic , strong) IBOutlet NSButton *json_btn;
+@property (weak) IBOutlet NSButton *swift_Btn;
+
+
+
+@end
